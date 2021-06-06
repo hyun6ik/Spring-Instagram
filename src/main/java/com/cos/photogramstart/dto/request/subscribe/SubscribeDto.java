744ef2166dcbb01e689fb73fe6218dae8c6c90b6
DESKTOP-NQ23D5Q\Hyunsik <fkanwm@phpschool.com>
2021-06-06 16:43:15 +0900

    조인과 스칼라 서브쿼리

---
+++ src/main/java/com/cos/photogramstart/dto/request/subscribe/SubscribeDto.java
@@ -0,0 +1,19 @@
+package com.cos.photogramstart.dto.request.subscribe;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SubscribeDto {
+
+    private Long id;
+    private String username;
+    private String profileImageUrl;
+    private Integer subscribeState;
+    private Integer equalUserState;
+}
